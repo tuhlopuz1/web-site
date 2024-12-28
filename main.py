5f0c8ecfcaae8fe065f4a6fea54fc84c20e2a0db
tuhlopuz1 <tuhlopuz1@gmail.com>
2024-12-28 21:09:43 +0300

    db adapter

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-import sqlite3
-connection = sqlite3.connect("localdb.db")
+from supabase import create_client, Client
 
-cursor = connection.cursor()
+_url = 'https://txrvpoufhekskfpnyrhn.supabase.co'
 
-cursor.execute(
-    "CREATE TABLE users(id INTEGER, email TEXT, password TEXT)"
-)
+_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4cnZwb3VmaGVrc2tmcG55cmhuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNTQwNjQ5MSwiZXhwIjoyMDUwOTgyNDkxfQ.HV9JxDADslVOH1ZJNDYR-OrQdhv-5IJKXlJ1MyEuZYU'
+
+class DBAdapter:
+    def __init__(self, url, key):
+        self.Client = create_client(url, key)
+
+    def get_user_by_email(self, email):
+        user_data = self.Client.table('users').select("*").eq('email', email).execute()
+        return user_data.data
+    def get_user_by_login(self, login):
+        user_data = self.Client.table('users').select("*").eq('login', login).execute()
+        return user_data.data
+    def insert_new_user(self, email, login, password):
+        new_user = {
+            'email': email,
+            'login': login,
+            'password': password
+        }
+        self.Client.table('users').insert(new_user).execute()
+
+adapter = DBAdapter(_url, _key)
+
+
