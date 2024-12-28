deeedbb09aea609e2ec4ddabcc332f77f02704f2
Finikfin <finikgrib@gmail.com>
2024-12-28 21:58:40 +0300

    ss

--- pages/log_in_page/log_in.js
+++ pages/log_in_page/log_in.js
@@ -6,20 +6,44 @@ const password = document.getElementById('password');
 
 function buttonclick(){
     console.log('тык');
-    numb = input.value;
-    fetch('http://localhost:5000/'+numb)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        console.log(response);
-        return response.json(); // Преобразуем ответ в JSON
-    })
-    .then(data => {
-        console.log(data); // Обрабатываем полученные данные
-        text.innerText = data;
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
+    log_or_not = login.value;
+    if (log_or_not.includes("@")){
+        fetch('http://localhost:5000/get-user-by-email/'+log_or_not)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            console.log(response);
+            return response.json(); // Преобразуем ответ в JSON
+        })
+        .then(data => {
+            console.log(data); // Обрабатываем полученные данные
+            text.innerText = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+
+    } else {
+        fetch('http://localhost:5000/get-user-by-login/'+log_or_not)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            console.log(response);
+            return response.json(); // Преобразуем ответ в JSON
+        })
+        .then(data => {
+            console.log(data); // Обрабатываем полученные данные
+            text.innerText = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+
+    }
+
+
+
+
 }
