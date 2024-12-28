789991eef1dd96130fa14416f2a9bf027d4d8513
tuhlopuz1 <tuhlopuz1@gmail.com>
2024-12-28 23:30:38 +0300

    debug

--- pages/log_in_page/log_in.js
+++ pages/log_in_page/log_in.js
@@ -1,11 +1,14 @@
-const button = document.getElementById('button');
+const button = document.getElementById('submit_button');
 const login = document.getElementById('login');
 const password = document.getElementById('password');
-
+const message = document.getElementById('message');
 
 
 function buttonclick(){
-    console.log('тык');
+    
+    
+    console.log('кнопка нажата');
+    let res_data = [];
     log_or_not = login.value;
     if (log_or_not.includes("@")){
         fetch('http://localhost:5000/get-user-by-email/'+log_or_not)
@@ -13,12 +16,13 @@ function buttonclick(){
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            console.log(response);
+            
             return response.json(); // Преобразуем ответ в JSON
         })
         .then(data => {
-            console.log(data); // Обрабатываем полученные данные
-            text.innerText = data;
+            
+            res_data = data
+            log_in(res_data);
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
@@ -30,20 +34,44 @@ function buttonclick(){
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            console.log(response);
+            
             return response.json(); // Преобразуем ответ в JSON
         })
         .then(data => {
-            console.log(data); // Обрабатываем полученные данные
-            text.innerText = data;
+            
+            res_data = data
+            log_in(res_data);
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
         });
+
+
+        
+
 
     }
 
 
 
 
+}
+
+
+function log_in(data){
+    console.log(data);
+    
+    if (Array.isArray(data) && data.length === 0){
+        message.innerText = 'Неправильный логин или пароль';
+        return 0;
+    }
+    pd = data[0]['password']
+    console.log(pd);
+    console.log(password.value)
+    if (pd == password.value){
+        message.innerText = 'Успешный вход';
+    }
+    else{
+        message.innerText = 'Неправильный логин или пароль';
+    }
 }
