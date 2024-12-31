1ceb0ae12b1d3ee5746c088fd5de0bbf70290b57
tuhlopuz1 <tuhlopuz1@gmail.com>
2024-12-31 17:43:57 +0300

    промежуточные изменения

--- pages/home_page/home.js
+++ pages/home_page/home.js
@@ -1,4 +1,17 @@
 document.getElementById('user-button').addEventListener('click', () => {
-    const dropdown = document.getElementById('dropdown-menu');
-    dropdown.classList.toggle('hidden');
-  });
+  const dropdown = document.getElementById('dropdown-menu');
+  dropdown.classList.toggle('hidden');
+});
+
+function showLoader() {
+  document.getElementById('loader').classList.remove('hidden');
+}
+
+function hideLoader() {
+  document.getElementById('loader').classList.add('hidden');
+}
+
+
+window.onload = function() {
+  hideLoader(); // Скрыть загрузчик после загрузки страницы
+};
---
+++ pages/my_quizzes_page/my_quizzes.js
@@ -0,0 +1,4 @@
+document.getElementById('user-button').addEventListener('click', () => {
+    const dropdown = document.getElementById('dropdown-menu');
+    dropdown.classList.toggle('hidden');
+  });
